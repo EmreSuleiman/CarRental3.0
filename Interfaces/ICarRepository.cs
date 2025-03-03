9bbc1b409a532340b1965d6419fe344c2367e0c6
sepro <sepro@DESKTOP-B9C8CPV>
2025-03-03 11:05:07 +0200

    Add project files.

---
+++ Interfaces/ICarRepository.cs
@@ -0,0 +1,16 @@
+﻿using CarRental3._0.Models;
+
+namespace CarRental3._0.Interfaces
+{
+    public interface ICarRepository
+    {
+        Task<IEnumerable<Car>> GetAll();
+        Task<Car> GetByIdAsync(int id);
+        Task<Car> GetByIdAsyncNotracking(int id);
+        Task<IEnumerable<Car>> GetCarByCategory(string category);
+        bool Add(Car car);
+        bool Update(Car car);
+        bool Delete(Car car);
+        bool Save();
+    }
+}
